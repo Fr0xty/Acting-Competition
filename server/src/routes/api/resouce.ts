cfc2864f513ec02174074fa639135a55a27130e7
Fr0xty <ongjiae8@gmail.com>
2022-08-03 17:52:52 +0800

    added /@me route to get user info

--- server/src/routes/api/resouce.ts
+++ server/src/routes/api/resouce.ts
@@ -2,4 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
+import userRouter from './resource/user.js';
+
+router.use(userRouter);
+
 export default router;
---
+++ server/src/routes/api/resource/user.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import accessTokenCheck from '../../../middlewares/accessTokenCheck.js';
+import { sqlGetUserWithAccessToken } from '../../../utils/sqlAcount.js';
+
+const router = Router();
+
+/**
+ * get all information about current logged in user
+ */
+router.get('/@me', accessTokenCheck, async (req, res) => {
+    /**
+     * get info from sql
+     */
+    const userInfo = await sqlGetUserWithAccessToken(req.accessToken!);
+    if (!userInfo) return res.send(401);
+
+    /**
+     * prevent password from getting sent
+     */
+    userInfo.password = '';
+    res.json(userInfo);
+});
+
+export default router;
