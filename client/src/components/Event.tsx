29a847c3603fca0b17af6db9befc20ae9f4ab686
Fr0xty <ongjiae8@gmail.com>
2022-08-11 18:55:48 +0800

    added EventDetails

--- client/src/components/Event.tsx
+++ client/src/components/Event.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import EventDetails from './EventDetails';
 
 interface EventProperties {
     userType: 'admin' | 'participant' | 'judge';
@@ -6,14 +7,38 @@ interface EventProperties {
 }
 
 const Event = ({ userType, eventId }: EventProperties) => {
+    const [eventData, setEventData] = useState<{ eventDetail: any; eventUsers: any[] | null }>({
+        eventDetail: null,
+        eventUsers: null,
+    });
+    const [eventStatus, setEventStatus] = useState<null | 'starting' | 'ongoing' | 'ended'>(null);
+
+    const processEventStatus = () => {
+        if (!eventData.eventDetail) return;
+
+        const { register_deadline: registerDeadline, event_deadline: eventDeadline } = eventData.eventDetail;
+        console.log(eventData);
+
+        if (new Date(eventDeadline) < new Date()) return setEventStatus('ended');
+        if (new Date(registerDeadline) < new Date()) return setEventStatus('ongoing');
+        setEventStatus('starting');
+    };
+
     useEffect(() => {
         (async () => {
             const getEventInfoReq = await fetch(`/api/resource/get-event?event-id=${eventId}`);
-            console.log(await getEventInfoReq.json());
+            if (getEventInfoReq.status !== 200) return;
+
+            setEventData(await getEventInfoReq.json());
+            processEventStatus();
         })();
     }, [eventId]);
 
-    return <div className="event">{eventId}</div>;
+    return (
+        <div className="event">
+            <EventDetails eventStatus={eventStatus} eventDetail={eventData.eventDetail} />
+        </div>
+    );
 };
 
 export default Event;
---
+++ client/src/components/EventDetails.tsx
@@ -0,0 +1,49 @@
+import '../styles/EventDetails.scss';
+
+interface EventDetailsProperties {
+    eventDetail: {
+        description: string;
+        event_deadline: string;
+        event_id: string;
+        name: string;
+        register_deadline: string;
+    } | null;
+
+    eventStatus: null | 'starting' | 'ongoing' | 'ended';
+}
+
+const EventDetails = ({ eventDetail, eventStatus }: EventDetailsProperties) => {
+    return (
+        <div className="event-details">
+            {eventDetail === null && <p>loading..</p>}
+
+            {eventDetail && (
+                <>
+                    <h2>{eventDetail.name}</h2>
+                    <p>{eventDetail.description}</p>
+
+                    <div className="dates">
+                        <span className="register">
+                            Register Deadline:
+                            <span className={`date ${eventStatus === 'starting' ? 'success' : 'danger'}`}>
+                                {eventDetail.register_deadline}
+                            </span>
+                        </span>
+                        <span className="event">
+                            Event Ending:
+                            <span
+                                className={`date ${
+                                    eventStatus !== 'starting' ? (eventStatus === 'ongoing' ? 'success' : 'danger') : ''
+                                }`}
+                            >
+                                {eventDetail.event_deadline}
+                            </span>
+                        </span>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default EventDetails;
