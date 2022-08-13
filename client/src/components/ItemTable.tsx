aedd0aff46bf289d5cd6e1fc0ae274159ce4ee37
Fr0xty <ongjiae8@gmail.com>
2022-08-13 18:21:53 +0800

    added item page content when no eventid is provided

---
+++ client/src/components/ItemTable.tsx
@@ -0,0 +1,19 @@
+const ItemTable = () => {
+    return (
+        <div className="item-table">
+            <div className="table">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Item</td>
+                            <td>Judge</td>
+                        </tr>
+                    </thead>
+                    <tbody>{}</tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ItemTable;
--- client/src/pages/userPages/Item.tsx
+++ client/src/pages/userPages/Item.tsx
@@ -1,11 +1,37 @@
+import '../../styles/Item.scss';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import ItemTable from '../../components/ItemTable';
 import UserNavbar from '../../components/UserNavbar';
 import UserPageNavigation from '../../components/UserPageNavigation';
 
 const Item = () => {
+    const [searchParams] = useSearchParams();
+
+    const [eventId, setEventId] = useState<null | string>('');
+    const [items, setItems] = useState('');
+
+    useEffect(() => {
+        setEventId(searchParams.get('event-id'));
+
+        (async () => {
+            // const itemReq = await fetch(`/api/resource/items?event-id=${}`);
+        })();
+    }, []);
     return (
         <div className="item">
             <UserNavbar />
             <UserPageNavigation userType="admin" currentPage="item" />
+            {eventId && <ItemTable />}
+            {!eventId && (
+                <div className="no-event-id">
+                    <h1>This page is to edit items.</h1>
+                    <p>
+                        Please use the <button>Edit Items</button> button in event page to edit.
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
