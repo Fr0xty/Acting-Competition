1addb92b2d713acce20e58b8b1dac3ddf445c252
Fr0xty <ongjiae8@gmail.com>
2022-08-22 20:41:56 +0800

    added home page content

--- client/src/pages/userPages/HomePage.tsx
+++ client/src/pages/userPages/HomePage.tsx
@@ -1,3 +1,5 @@
+import '../../styles/HomePage.scss';
+
 import UserPageNavigation from '../../components/UserPageNavigation';
 import UserNavbar from '../../components/UserNavbar';
 
@@ -7,9 +9,30 @@ interface HomePageProperties {
 
 const HomePage = ({ userType }: HomePageProperties) => {
     return (
-        <div className="admin-home">
+        <div className="home-page">
             <UserNavbar />
             <UserPageNavigation userType={userType} currentPage="home" />
+
+            <div className="home-page-welcome-text">
+                {userType === 'admin' && (
+                    <>
+                        <h1>You're an Admin!</h1>
+                        <p>Your job is to manage events, items, accounts and approve marks given by the judges.</p>
+                    </>
+                )}
+                {userType === 'participant' && (
+                    <>
+                        <h1>You're a Participant!</h1>
+                        <p>You can join events and view marks given by the judges.</p>
+                    </>
+                )}
+                {userType === 'judge' && (
+                    <>
+                        <h1>You're a Judge!</h1>
+                        <p>Your job is to give marks for your assigned items.</p>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
