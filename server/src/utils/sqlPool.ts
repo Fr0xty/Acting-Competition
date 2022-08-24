ce5a0d482e4957c13ca16b7cc0a8f49ed1432b8b
Fr0xty <ongjiae8@gmail.com>
2022-08-24 17:53:57 +0800

    added comment for easier time of documenting

--- server/src/utils/sqlPool.ts
+++ server/src/utils/sqlPool.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config';
-
 import { createPool } from 'mysql2/promise';
 
+/**
+ * instance of connection to mysql server
+ */
 const pool = createPool({
     user: process.env.USER,
     password: process.env.PASSWORD,
